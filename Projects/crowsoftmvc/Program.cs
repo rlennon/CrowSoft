0ab2d5e0b363bd8d25667c31cd966324866cb9ed
Charles Aylward <L00131535@student.lyit.ie>
2019-04-03 11:04:27 +0100

    After run commit

--- Projects/crowsoftmvc/Program.cs
+++ Projects/crowsoftmvc/Program.cs
@@ -21,8 +21,8 @@ namespace crowsoftmvc
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
-		        .UseUrls("http://*:5123")
-		        .UseContentRoot(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+		        //.UseUrls("http://*:5123")
+		        //.UseContentRoot(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
                 .UseStartup<Startup>();
     }
 }
