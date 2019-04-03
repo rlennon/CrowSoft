25413ed1a06e69a45fee1eae6d1c9f9e11c2d08d
Charles Aylward <L00131535@student.lyit.ie>
2019-04-03 09:46:22 +0100

    Updates to documents and code

---
+++ Projects/crowsoftmvcv1/Controllers/DummyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crowsoftmvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crowsoftmvc.Controllers
+{
+    public class DummyController : Controller
+    {
+        public IActionResult Index()
+        {
+            CrowsoftContext context = HttpContext.RequestServices.GetService(typeof(CrowsoftContext)) as CrowsoftContext;
+            List<Dummy> list = context.GetAllDummys();
+            return View(list);
+        }
+    }
+}
---
+++ Projects/crowsoftmvcv1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace crowsoftmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
