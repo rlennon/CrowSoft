60a2d556876cf204d8204b20c45645d2bd3a6435
Charles Aylward <L00131535@student.lyit.ie>
2019-04-07 13:34:07 +0100

    Clean up and added comments to the Unit Test project

--- Projects/crowsoft_unittests/UnitTest1.cs
+++ Projects/crowsoft_unittests/UnitTest1.cs
@@ -11,22 +11,22 @@ namespace Tests
     {
         private string connection_string;
 
+        // This sets up the test and reads the connectionstring from the appconfig.json file
         [SetUp]
         public void Setup()
         {
             var config = new ConfigurationBuilder()
                 .AddJsonFile("appconfig.json")
                 .Build();
-            //connection_string = ConfigurationManager.AppSettings["ConnectionString"];
+
             connection_string = config["CONNECTION_STRING"];
         }
 
+        // This is a example test, that test if Dummy records are available in the MySQL Database
         [Test]
         public void Test_GetDummyList()
         {
-            //DummyContext context = new DummyContext("server=172.28.25.133;port=3306;database=crowsoftdb;uid=dbadmin;password=csoftsql");
             DummyContext context = new DummyContext(connection_string);
-
             List<crowsoftmvc.Models.Dummy> myDummyList = context.GetAllDummys();
 
             Assert.Greater(myDummyList.Count, 0, "Error No Dummy Records Returned");
