d0948819bcd8a7209a8df56f6eb7df3d63f0f72d
Charles Aylward <L00131535@student.lyit.ie>
2019-04-20 12:51:32 +0100

    Update to config file add chrome driver path

--- Projects/crowsoft_seleniumtests/UnitTest1.cs
+++ Projects/crowsoft_seleniumtests/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace Tests
     public class Tests
     {
         private string webapp_url;
+        private string chrome_path;
         [SetUp]
         public void Setup()
         {
@@ -18,6 +19,7 @@ namespace Tests
                 .Build();
 
             webapp_url = config["WEB_URL"];
+            chrome_path = config["CHROME_DRIVER_PATH"];
         }
 
         // This Test method will test if the web site can be opened and click on the Register button
@@ -26,10 +28,8 @@ namespace Tests
         {
             try
             {
-                // Get the default base directory where the test app is running
-                var path = AppDomain.CurrentDomain.BaseDirectory;
                 // Instatiate web driver for Selenium and use the Chrome Drive
-                IWebDriver driver = new ChromeDriver(path);
+                IWebDriver driver = new ChromeDriver(chrome_path);
                 // Navigate to the CrowSoft Dev server. Note the webapp_url are in the appconfig.json file
                 driver.Navigate().GoToUrl(webapp_url);
                 Console.WriteLine("Opened web application successfully");
