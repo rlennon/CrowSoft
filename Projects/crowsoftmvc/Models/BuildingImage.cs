9a064608805d783041320de55bc97137ddde5626
Charles Aylward <L00131535@student.lyit.ie>
2019-04-28 19:27:13 +0100

    Added Data Annotations

--- Projects/crowsoftmvc/Models/BuildingImage.cs
+++ Projects/crowsoftmvc/Models/BuildingImage.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace crowsoftmvc.Models
 {
     public partial class BuildingImage
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdBuildingImage { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "{0} is required.")]
         public string Description { get; set; }
+
+        [Display(Name = "Select Image Folder")]
+        [StringLength(200)]
+        [Required(ErrorMessage = "{0} is required.")]
         public string ImagePath { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        [Column("BuildingQuoteIdBuildingQuote")]
+        [ForeignKey("BuildingQuote")]
         public int BuildingQuoteIdBuildingQuote { get; set; }
 
+        [NotMapped]
         public virtual BuildingQuote BuildingQuoteIdBuildingQuoteNavigation { get; set; }
     }
 }
--- Projects/crowsoftmvc/Models/BuildingQuote.cs
+++ Projects/crowsoftmvc/Models/BuildingQuote.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace crowsoftmvc.Models
 {
@@ -10,25 +14,87 @@ namespace crowsoftmvc.Models
             BuildingFeatures = new HashSet<BuildingFeatures>();
             BuildingImage = new HashSet<BuildingImage>();
         }
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdBuildingQuote { get; set; }
+
+        [Column("UserAccountIdUserAccount")]
+        [ForeignKey("UserAccount")]
+        [HiddenInput(DisplayValue = false)]
         public int UserAccountIdUserAccount { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "{0} is required.")]
         public string Description { get; set; }
+
+        [Display(Name = "Select a Measurement")]
+        [StringLength(20)]
+        [Required(ErrorMessage = "{0} is required.")]
         public string MeasurementType { get; set; }
+        [NotMapped]
+        public IEnumerable<SelectListItem> MeasureMentsList
+        {
+            get
+            {
+                return Measurements.GetMeasurements();
+            }
+
+            set => Measurements.GetMeasurements();
+        }
+
+        [Display(Name = "Height")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public int Height { get; set; }
+
+        [Display(Name = "Width")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public int Width { get; set; }
+
+        [Display(Name = "Depth")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public int Depth { get; set; }
+
+        [Display(Name = "Purpose of the Building")]
+        [StringLength(150)]
+        [Required(ErrorMessage = "{0} is required.")]
         public string PurposeOfBuilding { get; set; }
+
+        [Display(Name = "Building Size in Sqr Meters")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public int? BuildingSize { get; set; }
+
+        [Display(Name = "Total Cost")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        [Range(0, 9999999999999999.99)]
         public decimal? TotalCost { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public DateTime? DateCreated { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public string Status { get; set; }
+
+        [Display(Name = "Timeframe of Project (Months)")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public int? TimeFrame { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public DateTime? DateUpdated { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public int? UpdatedById { get; set; }
 
+        [NotMapped]
+        [HiddenInput(DisplayValue = false)]
         public virtual UserAccount UserAccountIdUserAccountNavigation { get; set; }
+
+       // [ForeignKey("BuildingQuote_idBuildingQuote")]
         public virtual ICollection<BuildingFeatures> BuildingFeatures { get; set; }
+
+       // [ForeignKey("BuildingQuote_idBuildingQuote")]
         public virtual ICollection<BuildingImage> BuildingImage { get; set; }
     }
 }
